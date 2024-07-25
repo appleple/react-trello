06b95c84458be305f45fb87cc72aebc64fa5a977
宇井 陸登 <ui@appleple.com>
2024-07-25 20:35:18 +0900

    react 17までに対応

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -20,8 +20,6 @@ module.exports = {
           loader: "style-loader" // creates style nodes from JS strings
         }, {
           loader: "css-loader" // translates CSS into CommonJS
-        }, {
-          loader: "sass-loader" // compiles Sass to CSS
         }],
         include: path.resolve(__dirname, '../')
       },
